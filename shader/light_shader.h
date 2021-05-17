b6897138f167939f6621fbeb19da9e9ae7affca3
shenxin <1683137709@qq.com>
2021-05-17 19:41:44 +0800

    Delete the Vertex Color field of the Light Shader class

--- shader/light_shader.h
+++ shader/light_shader.h
@@ -1,8 +1,6 @@
 #pragma once
 
 struct LightShader : public ShaderBase {
-	std::array<vec3, 3> vertex_color;
-public:
 	virtual vec4 vertex(const Vertex &vertex, int idx) override;
 	virtual bool fragment(std::array<Vertex *, 3> &vertices, vec3 &color) override;
 };
