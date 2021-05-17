2320cb34c90c9b607783ea721e2463535e2a8302
shenxin <1683137709@qq.com>
2021-05-17 19:02:33 +0800

    add the soft_rendere class

--- include/soft_renderer.h
+++ include/soft_renderer.h
@@ -1,7 +1,15 @@
 #pragma once
 
-
 class SoftRenderer {
-	Window		 window;
-	FrameBuffer	 frame;
+	Window						window;
+	FrameBuffer					frame;
+	std::shared_ptr<CameraBase> camera_ptr;
+	std::shared_ptr<ShaderBase> shader_ptr;
+	std::shared_ptr<Model>		model_ptr;
+public:
+	SoftRenderer(int _width, int _height, std::shared_ptr<CameraBase> _camera_ptr, 
+				 std::shared_ptr<ShaderBase> _shader_ptr, std::shared_ptr<Model> _model_ptr);
+	~SoftRenderer() = default;
+public:
+	void normal_renderer();	
 };
--- src/soft_renderer.cpp
+++ src/soft_renderer.cpp
@@ -1,1 +1,11 @@
 #include "common.h"
+
+SoftRenderer::SoftRenderer(int _width, int _height, 
+						  std::shared_ptr<CameraBase> _camera_ptr, 
+						  std::shared_ptr<ShaderBase> _shader_ptr, 
+						  std::shared_ptr<Model> _model_ptr) 
+: window(_width, _height, "MySoftRenderer"), frame(_width, _height)
+, camera_ptr(_camera_ptr), shader_ptr(_shader_ptr), model_ptr(_model_ptr) {
+
+	// TODO 增加窗口事件回调函数
+}
