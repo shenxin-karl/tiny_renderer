4881c8f47893c8714dbab110caee0df5aec94a6b
shenxin <1683137709@qq.com>
2021-05-27 00:11:07 +0800

    add cutting code 3

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -16,6 +16,16 @@ Vertex Vertex::operator+(const Vertex &other) const noexcept {
 	};
 }
 
+
+void Vertex::perspective_divide() {
+	float inverse_w = 1 / position.w();
+	for (int i = 0; i < position.size() - 1; ++i)
+		position[i] *= inverse_w;
+
+	normal *= inverse_w;
+	texcoords *= inverse_w;
+}
+
 Texture::Texture(const std::string &_var, const std::string &_path)
 : var(_var), texture(_path) {
 
@@ -41,24 +51,25 @@ void Mesh::draw(FrameBuffer &frame, ShaderBase &shader) const {
 	}
 }
 
-struct VertexResult {
-	vec4	position;
-	int		index;
-};
+
 void Mesh::process_triangle(FrameBuffer &frame, ShaderBase &shader, std::array<int, 3> our_indices) const {
-	std::array<VertexResult, 3> vertex_res;
+	std::vector<Vertex> out_vertices;
+	std::vector<int>    out_indices;
+	out_vertices.reserve(12);
+	out_indices.reserve(12); 
 	for (int i = 0; i < 3; ++i) {
 		int index = our_indices[i];
 		const Vertex &vertex = vertices[index];
 		vec4 point = shader.vertex(vertex, i);
-		vertex_res[i] = { point, index };
+		out_vertices.push_back(Vertex {
+			point,
+			vertex.normal,
+			vertex.texcoords,
+		});
+		out_indices.push_back(i);
 	}
 
-	std::vector<Vertex> out_vertices;
-	std::vector<int>    out_indices;
-	out_vertices.reserve(3);
-	out_indices.reserve(3);
-	if (plane_cutting(out_vertices, out_indices) < 0)		// cutting
+	if (plane_cutting(out_vertices, out_indices) == 0)		// cutting
 		return;
 
 	for (auto &v : out_vertices)		
@@ -66,13 +77,18 @@ void Mesh::process_triangle(FrameBuffer &frame, ShaderBase &shader, std::array<i
 	
 	int limit = static_cast<int>(out_indices.size()) - 2;
 	for (size_t i = 0; i < limit; i += 3u) {
-		if (backface_culling(shader, out_vertices[i], out_vertices[i+1], out_vertices[i+2]))
+		int idx1 = out_indices[i];
+		int idx2 = out_indices[i+1];
+		int idx3 = out_indices[i+2];
+		if (idx1 < 0)
 			continue;
 
+		if (backface_culling(shader, out_vertices[idx1], out_vertices[idx2], out_vertices[idx3]))
+			continue;
 		Draw::triangle(frame, shader, {
-			&out_vertices[i],
-			&out_vertices[i+1],
-			&out_vertices[i+2],
+			&out_vertices[idx1],
+			&out_vertices[idx2],
+			&out_vertices[idx3],
 		});
 	}
 }
@@ -100,8 +116,8 @@ int Mesh::plane_cutting(std::vector<Vertex> &vertices, std::vector<int> &indices
 		{ 2, std::greater<float>(), },
 	};
 	int size = indices.size();
-	for (auto iter = std::begin(planes); iter != std::end(planes) && size > 0; ++iter) {
-		for (int i = 0; i < indices.size()-2 && size > 0; ++i) {
+	for (auto iter = std::begin(planes); iter != std::end(planes) && size != 0; ++iter) {
+		for (int i = 0; i < indices.size()-2 && size != 0; ++i) {
 			if (indices[i] < 0)
 				continue;
 
@@ -125,7 +141,7 @@ int Mesh::plane_cutting_triangle(std::vector<Vertex> &vertices,
 		bool is_outside = outside(v.position, compare_func, idx, 1.f);
 		if (is_outside) {
 			outside_flag[i] = true;
-			outside_indices[outside_size] = i;
+			outside_indices[outside_size] = vertex_idx;
 			++outside_size;
 		}
 		++i;
@@ -139,7 +155,23 @@ int Mesh::plane_cutting_triangle(std::vector<Vertex> &vertices,
 	}
 	case 1:
 	{
-		// todo case1
+		auto iter = std::find(outside_flag.begin(), outside_flag.end(), true);
+		assert(iter != outside_flag.end());
+		int offset = static_cast<int>(iter - outside_flag.begin());
+		int outside_idx = outside_indices[0];
+		int inside_idx1 = indices[(offset + 1) % 3];
+		int inside_idx2 = indices[(offset + 3 - 1) % 3];
+		const Vertex &outside_vertex = vertices[outside_idx];
+		const Vertex &inside_vertex1 = vertices[inside_idx1];
+		const Vertex &inside_vertex2 = vertices[inside_idx2];
+
+		// 插值第一个
+		float t1 = get_plane_ratio(inside_vertex1, outside_vertex, idx);
+		Vertex v1 = interp_vertex(inside_vertex1, outside_vertex, t1);
+		// 插值第二个顶点
+		float t2 = get_plane_ratio(inside_vertex2, outside_vertex, idx);
+		Vertex v2 = interp_vertex(inside_vertex2, outside_vertex, t2);
+
 		return 3;
 	}
 	case 2:
